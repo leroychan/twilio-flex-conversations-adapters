9821d92c6f633df86b1d2c7a2be7d0591be09663
Leroy Chan <5236195+leroychan@users.noreply.github.com>
2023-07-11 13:26:44 +0800

    fix: folder structure

--- plugin-conversations-icons/src/components/SocialIcons.tsx
+++ plugin-conversations-icons/src/components/SocialIcons.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export const DiscordIcon = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 127.14 96.36">
--- test.js
+++
@@ -1,7 +0,0 @@
-const {
-  parseExampleEnvironmentVariables,
-  printContextVariables,
-} = require("./scripts/common.js");
-
-const test = parseExampleEnvironmentVariables();
-printContextVariables(test);
