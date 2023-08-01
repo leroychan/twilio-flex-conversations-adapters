e0f65f98e417008724c02e0b153d6820c11a876b
Leroy Chan <5236195+leroychan@users.noreply.github.com>
2023-08-02 00:04:41 +0800

    fix: googlechat env variables

--- serverless-functions/src/functions/api/googlechat/googlechat.helper.private.ts
+++ serverless-functions/src/functions/api/googlechat/googlechat.helper.private.ts
@@ -306,15 +306,19 @@ export const getGoogleChatClient = async (
     let credentials;
     console.log(Runtime.getAssets());
     console.log(Object.keys(Runtime.getAssets()).length);
+    console.log("Inside 0");
     if (
       Object.keys(Runtime.getAssets()).length !== 0 &&
       Object.keys(Runtime.getAssets()[`/${googleChatCredentialsFileName}`])
         .length !== 0
     ) {
+      console.log("Inside 1");
       // -- Priority 1: Use Private Asset File
       const rawCredentials =
         Runtime.getAssets()[`/${googleChatCredentialsFileName}`].open;
+      console.log("Inside 2");
       const rawCredentialsContent = rawCredentials();
+      console.log("Inside 3");
       credentials = JSON.parse(rawCredentialsContent);
     } else {
       // -- Priority 2: Use Environment Variable
